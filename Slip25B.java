ff7e3cdc60f92595a6ebbfad2b48503c69e9dba8
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-07-24 09:13:16 +0530

    new

--- Slip25B.java
+++ Slip25B.java
@@ -1,4 +1,6 @@
 import series.*;
+import java.util.*;
+import java.util.Scanner;
 public class Slip25B
 {
 	public static void main(String arg[])
@@ -6,10 +8,12 @@ public class Slip25B
 		fibbo fob= new fibbo();
 		fob.fibbo_series(10);
 		sqaure sob= new sqaure();
-		int n=sob.funsqu(10);
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int n=sob.funsqu(num);
 		System.out.print("\n the sqaure of number : "+n);
 		cube cob= new cube();
-		int m=cob.funcube(10);
+		int m=cob.funcube(num);
 		System.out.print("\n the cube of number : "+m);
 	}
 }
