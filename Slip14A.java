fbaf8b96ada0df8c9713331c8c86d53d5a3063f0
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-08-01 10:34:41 +0530

    new

--- Slip14A.java
+++ Slip14A.java
@@ -1,4 +1,21 @@
-public class Slip14A 
+import java.awt.Frame;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Slip14A extends Frame implements ActionListener
 {
-    
+    Label l1,l2,l3;
+    TextField t1,t2,t3;
+    Button b1;
+    Frame f1;
+    TextArea t;
+    Slip14A()
+    {
+        l1 = new Label("Enter the eno");
+        l2 = new Label("enter the ename ");
+        l3 = new Label(" enter the salary");
+        
+    }
+
 }
