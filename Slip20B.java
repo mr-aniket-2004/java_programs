4373a0cfa3093fa0aa5f15693a9c26e04fb91898
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-08-01 10:00:04 +0530

    new

---
+++ Slip20B.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class public Slip20B extends Frame implements windowListener
+{
+    Slip20B()
+    {
+        Color c = new Color.red;
+        setColor(c);
+        setvisible(true);
+        setSize(400,400);
+        setLayout(new FlowLayout());
+    }
+}
