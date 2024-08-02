f7cd4ba2cf036c98f737c4d00ede6ad267f9a5f2
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-08-02 12:26:40 +0530

    new

--- Slip14A.java
+++ Slip14A.java
@@ -1,5 +1,3 @@
-import java.awt.Frame;
-import java.awt.event.ActionListener;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -15,7 +13,37 @@ public class Slip14A extends Frame implements ActionListener
         l1 = new Label("Enter the eno");
         l2 = new Label("enter the ename ");
         l3 = new Label(" enter the salary");
-        
+        t1 = new TextField(20);
+        t2 = new TextField(20);
+        t3 = new TextField(20);
+        t = new TextArea();
+        f1 = new Frame();
+        b1 = new Button("click here");
+        b1.addActionListener(this); 
+        add(l1); add(t1);
+        add(l2); add(t2);
+        add(l3); add(t3);
+        add(b1);
+        f1.add(t);
+        setSize(500,500);
+        setVisible(true);
+        setLayout(new FlowLayout());
+        f1.setSize(500,500);
+        f1.setLayout(new FlowLayout());
+        Font f = new Font("Arial",Font.BOLD,30);
+        setFont(f);
+        f1.setFont(f);
     }
 
+    public void actionPerformed(ActionEvent e)
+    {
+        String str;
+        str = "emp no :" + t1.getText()+"\n Emp name : "+ t2.getText() + "\n salary : "+ t3.getText();
+        t.setText(str);
+        f1.setVisible(true);
+        
+    }
+    public static void main(String[] args) {
+        Slip14A ob= new Slip14A();
+    }
 }
