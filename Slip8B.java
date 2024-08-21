a3f818c005bd553fda9b14e376f2f0d93d0234e6
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-08-21 10:32:15 +0530

    new

---
+++ Slip8B.java
@@ -0,0 +1,46 @@
+import java.util.*;
+abstract class Shape {
+    final float pi = 3.14f;
+    abstract void area();
+}
+
+
+class circle extends Shape{
+    float redious =0.0f;
+    circle(float r)
+    {
+        redious = r;
+        
+    }
+     public void area()
+    {
+        float result = pi*redious*redious;
+        System.out.println("the area of cicle : "+result);
+    }
+}
+
+class sphere extends Shape
+{   
+    float r = 0.0f;
+    sphere(float a)
+    {
+        r=a;
+    }
+    public void area()
+    {
+        float res = 4*pi*r*r;
+        System.out.println("the area of sphere : "+res);
+    }
+}
+
+public class Slip8B {
+    
+        public static void main(String arg[])
+    {
+        circle c1 = new circle(3.5f);
+        c1.area();
+
+        sphere s1 = new sphere(3.2f);
+        s1.area();
+    }
+}
