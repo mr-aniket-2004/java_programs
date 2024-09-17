2a8cbaefbee2eedd9748cbd713658b7356e16e7e
mr-aniket-2004 <157163406+mr-aniket-2004@users.noreply.github.com>
2024-09-17 21:46:32 +0530

    new

--- Slip12B.java
+++ Slip12B.java
@@ -1,30 +1,31 @@
 
-import  java.util.*;
+import java.util.*;
 
 public class Slip12B {
     public static void main(String[] args) {
         String ar[] = new String[20];
         String result[] = new String[20];
         Scanner sc = new Scanner(System.in);
-        
-        for(int i = 0; i<3; i++)
-        {
+
+        for (int i = 0; i < 3; i++) {
             String str = sc.nextLine();
             ar[i] = str;
         }
+        System.out.println("the original string ");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("\n \t"+ar[i]);
 
-        for(int i = 0; i<3; i++)
-        {
-           System.out.print("the original string "+ar[i]);
-           StringBuffer strb = new StringBuffer(ar[i]);
-           strb = strb.reverse();
-           System.out.println("\t revser string "+strb);
         }
+        System.out.println("\nThe Revers String");
+        for (int i = 0; i < 3; i++) {
+            StringBuffer strb = new StringBuffer(ar[i]);
+            strb = strb.reverse();
+            System.out.println("\n\t"+strb);
+        }
+        System.out.println("\n the revers string array  --");
+        for (int i = 2; i >= 0; i--) {
+            System.out.print( "\n\t"+ar[i]);
 
-        for(int i = 2; i>=0; i--)
-        {
-           System.out.print("\n the revers string array  --"+ar[i]);
-          
         }
-    }    
+    }
 }
