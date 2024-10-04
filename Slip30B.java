5964b6bf188cc78475a3a7a01b8152b4650854e2
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-10-04 12:25:22 +0530

    new

--- Slip30B.java
+++ Slip30B.java
@@ -1,3 +1,39 @@
+class NumberisZero extends Exception {
+    NumberisZero() {
+        System.out.println("Number is Zero");
+    }
+}
+
+class invalidData extends Exception {
+    invalidData() {
+        System.out.println("Invalid data");
+    }
+}
+
 public class Slip30B {
-    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        String s = args[0];
+        StringBuffer str = new StringBuffer(s);
+        try {
+            if (n == 0) {
+                throw new NumberisZero();
+            } else {
+                System.out.println("Original Number ---- "+n);
+                str = str.reverse();
+                if(str.equals(s))
+                {
+                    System.out.println("The number is palindrome");
+                }
+                else
+                {
+                    System.out.println("The number is not palindrome");
+                }
+            }
+        } catch (NumberisZero e) {
+            System.out.println("error found");
+        } finally {
+            System.out.println("complate the programme");
+        }
+    }
 }
