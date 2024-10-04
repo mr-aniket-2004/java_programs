3fab6383eb706585cc70f37bbda99e52838a6b43
mr-aniket-2004 <aniketchandiwadeprofessional@gmail.com>
2024-10-04 11:50:25 +0530

    hi

--- Slip29B.java
+++ Slip29B.java
@@ -1,22 +1,21 @@
+
+
 class noteligiable extends Exception {
     noteligiable() {
         System.out.println("Candidate is not eligiable for vote");
     }
 }
-class ArrayIndexOutOfBoundsException extends Exception
-{
-     ArrayIndexOutOfBoundsException()
-    {
-        System.out.println("plz enter the argument on command line ");
-    }
-}
+
 
 public class Slip29B {
     public static void main(String[] args)  {
-        int age = Integer.parseInt(args[0]);
         
         try
-        {
+        {   
+            if (args.length==0) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            int age = Integer.parseInt(args[0]);
             if(age<18)
             {
                 throw new noteligiable();
@@ -27,13 +26,13 @@ public class Slip29B {
             }
            
         }
+        catch(ArrayIndexOutOfBoundsException ee)
+        {
+            System.out.println("plz pass the Arguments");
+        }
         catch(noteligiable e)
         {
             System.out.println("the age must be 18 or above");
-        }
-        catch(ArrayIndexOutOfBoundsException exception)
-        {
-            System.out.println("plz pass the Arguments");
         }
         finally
         {
--- temp.java
+++ temp.java
@@ -1,7 +1,34 @@
-public class temp
-{
-    public static void main(String arg[0])
-    {
-        System.out.print("hello world ");
+class NotEligible extends Exception {
+    NotEligible() {
+        super("Candidate is not eligible for vote");
+    }
+}
+
+public class Slip29B {
+    public static void main(String[] args) {
+        try {
+            // Check if command line argument is provided
+            if (args.length == 0) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+
+            int age = Integer.parseInt(args[0]);
+
+            if (age < 18) {
+                throw new NotEligible();
+            } else {
+                System.out.println("Candidate is eligible for voting");
+            }
+
+        } catch (NotEligible e) {
+            System.out.println(e.getMessage());
+            System.out.println("The age must be 18 or above");
+        } catch (ArrayIndexOutOfBoundsException ee) {
+            System.out.println("Please pass the arguments");
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid number for age");
+        } finally {
+            System.out.println("Program run successfully");
+        }
     }
 }
